866c384ccccea572ace91fbba8c335d381d8a782
Ernesto <ebastidasp@unal.edu.co>
2020-10-03 23:46:09 -0500

    url

--- src/geosmart/information/server.js
+++ src/geosmart/information/server.js
@@ -1,3 +1,3 @@
-export const url = 'ec2-3-236-107-143.compute-1.amazonaws.com'
+export const url = 'ec2-18-206-15-172.compute-1.amazonaws.com'
 export const port = '2000'
 export const entryPoint = ''
