763cc19209922b2d757cca2a26ddb887ed5a026a
Esteban Jaramillo Sarmiento <26073885+Steve0929@users.noreply.github.com>
2020-10-06 22:21:21 -0500

    Update server.js

--- src/geosmart/userManagement/server.js
+++ src/geosmart/userManagement/server.js
@@ -1,4 +1,4 @@
-export const url = 'ec2-3-236-242-132.compute-1.amazonaws.com';
+export const url = 'ec2-35-173-250-160.compute-1.amazonaws.com';
 export const port = '4000';
 export const entryPointUser = 'user';
 export const entryPointUserLogin = 'user/login';
