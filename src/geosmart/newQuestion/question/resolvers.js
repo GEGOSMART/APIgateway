3063825ecaf9fa26a0d5a39c910387af5dcfb7b4
AndresFelipeCastellanos <afcastellanosn@unal.edu.co>
2020-10-06 22:48:54 -0500

    fixPath

--- src/geosmart/newQuestion/question/resolvers.js
+++ src/geosmart/newQuestion/question/resolvers.js
@@ -1,5 +1,5 @@
-import { generalRequest, getRequest } from '../../utilities';
-import { url, port, entryPoint } from './server';
+import { generalRequest } from '../../../utilities';
+import { url, port, entryPoint } from '../server';
 
 const URL = `https://${url}:${port}/${entryPoint}`;
 
