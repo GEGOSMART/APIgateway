39d7b3cc34169e5daa53f4b9c1b1b742e0f45c80
Ernesto <ebastidasp@unal.edu.co>
2020-10-11 16:56:45 -0500

    Cambio de tipo de dato

--- src/geosmart/information/score/typeDefs.js
+++ src/geosmart/information/score/typeDefs.js
@@ -1,29 +1,29 @@
 export const scoreTypeDef = `
   type score {
       ID: Int!
-      ID_User: Int!
+      ID_User: String!
       Score: Int!
       DatePlayed: String!
-      ID_Game: Int!
+      ID_Game: String!
   }
   type response{
       message: String!  
   }
   input scoreInput {
-      ID_User: Int!
+      ID_User: String!
       Score: Int!
       DatePlayed: String!
-      ID_Game: Int!
+      ID_Game: String!
   }`;
 
 export const scoreQueries = `
       allScores: [score]!
-      bestScoreByUserandGame(ID_User: Int!, ID_Game: Int!): [score]!
-      bestScoreByGame(ID_Game: Int!): [score]!
+      bestScoreByUserandGame(ID_User: String!, ID_Game: String!): [score]!
+      bestScoreByGame(ID_Game: String!): [score]!
   `;
 
 export const scoreMutations = `
     createScore(score: scoreInput!): response!
-    updateScore(ID: Int!, ID_User: Int!, Score: Int!, DatePlayed: String!, ID_Game: Int!): response!
+    updateScore(ID: Int!, ID_User: String!, Score: Int!, DatePlayed: String!, ID_Game: String!): response!
     deleteScore(ID: Int!): response!
 `;
