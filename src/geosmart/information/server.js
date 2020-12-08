4e2be173ad13ed8a2a4c8b6c6a2724bcc8d20507
afmoyar <afmoyar@unal.edu.co>
2020-12-07 19:24:15 -0500

    changing ports

--- src/geosmart/information/server.js
+++ src/geosmart/information/server.js
@@ -1,3 +1,3 @@
 export const url = '34.206.124.229'
-export const port = '2000'
+export const port = '30004'
 export const entryPoint = ''
--- src/geosmart/questions_n/server.js
+++ src/geosmart/questions_n/server.js
@@ -1,4 +1,4 @@
 export const url = '34.206.124.229';
-export const port = '5004';
+export const port = '30006';
 export const entryPointSession= 'trivia';
 export const entryPointKuestions= 'preguntas';
